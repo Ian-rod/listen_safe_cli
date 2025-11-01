22e9923153997d80b14c4c1d8fefae71c20afdb9
Ian-rod <ianomengo@gmail.com>
2025-11-01 14:46:07 +0300

    Added a bad words txt

--- src/main/java/ListenSafe.java
+++ src/main/java/ListenSafe.java
@@ -7,6 +7,9 @@ import java.net.http.HttpResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 public class ListenSafe {
     static String apiMainUrl="https://api.genius.com/";
 
@@ -19,7 +22,14 @@ public class ListenSafe {
     static String makeGetRequest(String searchString)
     {
         ///Make the Get request to genius
-    	searchString=searchString.replaceAll(" ", "");
+    	searchString= URLEncoder.encode(searchString, StandardCharsets.UTF_8);;
+    	
+    	//Get the list of words to filter
+    	List<String> wordsToFilter=getExplicitWords();
+    	if(wordsToFilter.size()==0)
+    	{
+    		return "Words to filter cannot be empty";
+    	}
         try { 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -32,6 +42,7 @@ public class ListenSafe {
         JSONObject jsonObj = new JSONObject(response.body());
         JSONObject jsonResponse = jsonObj.getJSONObject("response");
         JSONArray hits = jsonResponse.getJSONArray("hits");
+        
         
         // Iterate over hits
         for (int i = 0; i < hits.length(); i++) {
@@ -52,7 +63,8 @@ public class ListenSafe {
         	input=inputScanner.nextInt();
         	if(input==0.01)
         	{
-        		break;
+                inputScanner.close();
+        		return "User requested To exit";	
         	}
         	else if(input>= hits.length())
         	{
@@ -69,17 +81,20 @@ public class ListenSafe {
                     .build();
             HttpResponse<String> lyricResponse = lyricClient.send(lyricRequest, HttpResponse.BodyHandlers.ofString());
             String lyricsBody=lyricResponse.body().toLowerCase();
-            if(lyricsBody.contains("niggas"))
-            {
-            	System.out.println();
-            	System.out.println("The song "+SongName+" By "+ArtistName+" has the explicit word niggas");
-            }
-            else {
-            	 System.out.println("The song is clean and safe to Listen");
-            }
+            
+            for (String badword : wordsToFilter) {
+                if(lyricsBody.contains(badword))
+                {
+                	System.out.println();
+                	System.out.println("The song "+SongName+" By "+ArtistName+" has the explicit word "+badword);
+                }
+                else {
+                	/// System.out.println("The song is clean and safe to Listen");
+                }
+			}
+
         	System.out.println();
         }
-        inputScanner.close();
         } catch (Exception e) {
             System.out.println("Exception encountered : "+e.getMessage());
         }
@@ -88,7 +103,26 @@ public class ListenSafe {
     return "";
     }
     
+    static List<String> getExplicitWords()
+    {
+    	List<String> res=new ArrayList<String>();
+    
+    	///Currently using a txt file of all offensive words maybe add how to add words later
+    	try(BufferedReader reader = new BufferedReader(new FileReader(MyTokens.BadWordsSource))) {
+			///Open file
+    		   String line;
+            while ((line = reader.readLine()) != null) {
+                // Process each line here
+      			res.add(line);
+            }
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Encountered Error : "+e.getLocalizedMessage());
+		}
+    	return res;
+    }
     public static void main(String[] args) {
-        makeGetRequest("Russian Cream");
+       System.out.println( makeGetRequest("Glock In my lap"));
     }
 }
